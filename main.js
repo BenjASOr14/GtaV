0c3f37109fb30e24c3db75d327da8ed5af669824
BenjASOr14 <benjasoroza32@gmail.com>
2024-12-16 20:15:31 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+
+document.body.style.backgroundImage = "url('./VINEWOOD_2.png')";
+
+
+document.body.style.backgroundSize = 'cover';
+document.body.style.backgroundPosition = 'center';
+document.body.style.backgroundRepeat = 'no-repeat';
+
